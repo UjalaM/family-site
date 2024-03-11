f23d93b02427abd61f02a4165a590dd6ebee488d
ujalamaurya.um@gmail.com <45752599+AnchalM20@users.noreply.github.com>
2024-03-11 20:30:29 +0530

    client server package added

---
+++ server/src/index.js
@@ -0,0 +1,16 @@
+// import express from "express";
+
+// const server = express();
+
+// server.get("/", (req, res) => {
+//   res.json({ status: "success" });
+// });
+
+// server.listen(8080, () => {
+//   console.log("Server started");
+// });
+
+console.log("hello");
+console.log("world");
+// module: import
+// common: require
